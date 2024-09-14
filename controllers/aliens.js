eba0345e7d0a1c7a0d3b58f42deceb6168cd988a
Muhammad Talha <mtalhas960@gmail.com>
2024-09-14 16:28:51 +0500

    removed _v and _id from response and added images for aliens

--- controllers/aliens.js
+++ controllers/aliens.js
@@ -2,7 +2,7 @@ const Alien = require('../models/aliens');
 
 async function getAllAliens(req, res) {
     try {
-        const aliens = await Alien.find();
+        const aliens = await Alien.find({}, { _id: 0, __v: 0 });
         res.status(200).json(aliens);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -13,7 +13,7 @@ async function getRandomAlien(req, res) {
     try {
         const count = await Alien.countDocuments();
         const random = Math.floor(Math.random() * count);
-        const randomAlien = await Alien.findOne().skip(random);
+        const randomAlien = await Alien.findOne().skip(random).select('-_id -__v');
         res.status(200).json(randomAlien);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -22,7 +22,7 @@ async function getRandomAlien(req, res) {
 
 async function getAlienById(req, res) {
     try {
-        const alien = await Alien.findOne({ id: req.params.id });
+        const alien = await Alien.findOne({ id: req.params.id }, { _id: 0, __v: 0 });
         if (!alien) {
             return res.status(404).json({ message: 'Alien not found' });
         }
--- controllers/characters.js
+++ controllers/characters.js
@@ -2,7 +2,7 @@ const Character = require('../models/characters');
 
 async function getAllCharacters(req, res) {
     try {
-        const characters = await Character.find();
+        const characters = await Character.find({}, { _id: 0, __v: 0 });
         res.status(200).json(characters);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -13,7 +13,7 @@ async function getRandomCharacter(req, res) {
     try {
         const count = await Character.countDocuments();
         const random = Math.floor(Math.random() * count);
-        const randomCharacter = await Character.findOne().skip(random);
+        const randomCharacter = await Character.findOne().skip(random).select('-_id -__v');
         res.status(200).json(randomCharacter);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -22,7 +22,7 @@ async function getRandomCharacter(req, res) {
 
 async function getCharacterById(req, res) {
     try {
-        const character = await Character.findOne({ id: req.params.id });
+        const character = await Character.findOne({ id: req.params.id }, { _id: 0, __v: 0 });
         if (!character) {
             return res.status(404).json({ message: 'Character not found' });
         }
